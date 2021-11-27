3a52d74adcc3ba13a385f71859b37c9cec1b58c8
Romello Goodman <romellogood@gmail.com>
2021-11-26 23:25:16 -0500

    0.0.2

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 import {MersenneTwister19937, Random} from 'random-js';
 import SimplexNoise from 'simplex-noise';
 
-const getRandom = () => {
-  const seed = MersenneTwister19937.autoSeed();
+const getRandom = (randomSeed) => {
+  const seed = randomSeed || MersenneTwister19937.autoSeed();
 
   return new Random(seed);
 };
 
 const simplex = new SimplexNoise();
-const random = getRandom();
-
-const scale = 1;
-const AMPLITUDE = 5;
-const DAMPING = 0.1;
-const FREQUENCY = 0.001 / scale;
-const PARTICLE_COUNT = 1000;
-const PARTICLE_STEPS = 30 * scale;
-const STEP = 5 * scale;
 
 const noise2D = (simplex, x, y, frequency, amplitude) => {
   return simplex.noise2D(x * frequency, y * frequency) * amplitude;
@@ -49,16 +40,20 @@ const isInBound = (xCood, yCoord, width, height, margin) => {
   );
 };
 
-export const generateParticles = (width, height, margin = 0) => {
-  const {minWidth, maxWidth, minHeight, maxHeight} = getBounds(
-    width,
-    height,
-    margin
-  );
+export const generateParticles = ({
+  count,
+  height,
+  margin = 0.1,
+  seed,
+  width,
+}) => {
+  const random = getRandom(seed);
+  const bounds = getBounds(width, height, margin);
+  const {minWidth, maxWidth, minHeight, maxHeight} = bounds;
   let particles = [];
 
   // Generate some particles with a random position
-  for (let i = 0; i < PARTICLE_COUNT; i++) {
+  for (let i = 0; i < count; i++) {
     particles.push({
       x: random.real(minWidth, maxWidth),
       y: random.real(minHeight, maxHeight),
@@ -71,40 +66,64 @@ export const generateParticles = (width, height, margin = 0) => {
   return particles;
 };
 
-export const moveParticle = (particle) => {
+export const moveParticle = ({
+  amplitude,
+  damping,
+  frequency,
+  lengthOfStep,
+  particle,
+}) => {
   // Calculate direction from noise
-  const angle = noise2D(simplex, particle.x, particle.y, FREQUENCY, AMPLITUDE);
+  const angle = noise2D(simplex, particle.x, particle.y, frequency, amplitude);
 
   // Update the velocity of the particle based on the direction
-  particle.vx += Math.cos(angle) * STEP;
-  particle.vy += Math.sin(angle) * STEP;
+  particle.vx += Math.cos(angle) * lengthOfStep;
+  particle.vy += Math.sin(angle) * lengthOfStep;
 
   // Move the particle
   particle.x += particle.vx;
   particle.y += particle.vy;
 
   // Use damping to slow down the particle (think friction)
-  particle.vx *= DAMPING;
-  particle.vy *= DAMPING;
+  particle.vx *= damping;
+  particle.vy *= damping;
 
   particle.line.push([particle.x, particle.y]);
 };
 
-export const generateField = ({height, margin = 0, width}) => {
-  const particles = generateParticles(width, height, margin) || [];
+export const generateField = ({
+  amplitude = 5,
+  count = 1000,
+  damping = 0.1,
+  height,
+  margin = 0.1,
+  seed,
+  width,
+  scale = 1,
+} = {}) => {
+  const maxParticleSteps = 30 * scale;
+  const lengthOfStep = 5 * scale;
+  const frequency = 0.001 / scale;
+  const particles =
+    generateParticles({count, height, margin, seed, width}) || [];
 
-  // Filter after all of the steps
-  particles
-    ?.forEach((particle) => {
-      while (particle.line.length < PARTICLE_STEPS) {
-        moveParticle(particle);
-      }
-    })
-    ?.forEach((particle) => {
-      particle.line = particle.line.filter((particle) => {
-        return isInBound(particle[0], particle[1], width, height, margin);
+  particles?.forEach((particle) => {
+    while (particle.line.length < maxParticleSteps) {
+      moveParticle({
+        amplitude,
+        damping,
+        frequency,
+        lengthOfStep,
+        particle,
       });
+    }
+  });
+
+  particles?.forEach((particle) => {
+    particle.line = particle.line.filter((particle) => {
+      return isInBound(particle[0], particle[1], width, height, margin);
     });
+  });
 
   return particles;
 };
